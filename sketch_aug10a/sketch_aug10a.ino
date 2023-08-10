cbc29b6764e211fb4d41425291f254fd5e04778d
NaN The Wiser <90071713+RCarScar@users.noreply.github.com>
2023-08-10 13:15:41 +0430

    Goodbai World
    YASSIRRRR
    
    Co-Authored-By: TaiLe1602 <132976562+TaiLe1602@users.noreply.github.com>

---
+++ sketch_aug10a/sketch_aug10a.ino
@@ -0,0 +1,86 @@
+// Define motor control pins for Motor A
+const int motorAEnablePin = 11;   // Enable pin for Motor A
+const int motorAPin1 = 10;       // Control pin 1 for Motor A
+const int motorAPin2 = 9;       // Control pin 2 for Motor A
+
+// Define motor control pins for Motor B
+const int motorBEnablePin = 8;   // Enable pin for Motor B
+const int motorBPin1 = 7;        // Control pin 1 for Motor B
+const int motorBPin2 = 6;        // Control pin 2 for Motor B
+
+void setup() {
+  // Set the motor control pins as outputs for Motor A
+  pinMode(motorAEnablePin, OUTPUT);
+  pinMode(motorAPin1, OUTPUT);
+  pinMode(motorAPin2, OUTPUT);
+
+  // Set the motor control pins as outputs for Motor B
+  pinMode(motorBEnablePin, OUTPUT);
+  pinMode(motorBPin1, OUTPUT);
+  pinMode(motorBPin2, OUTPUT);
+
+}
+
+void loop() {
+  // Move Motor A forward and Motor B backward
+  digitalWrite(motorAEnablePin, HIGH); // Enable Motor A
+  digitalWrite(motorAPin1, HIGH);     // Set direction
+  digitalWrite(motorAPin2, LOW);
+
+  digitalWrite(motorBEnablePin, HIGH); // Enable Motor B
+  digitalWrite(motorBPin1, LOW);      // Set direction
+  digitalWrite(motorBPin2, HIGH);
+
+  delay(2000); // Run motors for 2 seconds
+
+  // Stop both motors
+  digitalWrite(motorAEnablePin, LOW); // Disable Motor A
+  digitalWrite(motorBEnablePin, LOW); // Disable Motor B
+
+  delay(1000); // Pause for 1 second
+
+  // Move Motor A backward and Motor B forward
+  digitalWrite(motorAEnablePin, HIGH); // Enable Motor A
+  digitalWrite(motorAPin1, LOW);      // Set direction
+  digitalWrite(motorAPin2, HIGH);
+
+  digitalWrite(motorBEnablePin, HIGH); // Enable Motor B
+  digitalWrite(motorBPin1, HIGH);     // Set direction
+  digitalWrite(motorBPin2, LOW);
+
+  delay(2000); // Run motors for 2 seconds
+
+  // Stop both motors
+  digitalWrite(motorAEnablePin, LOW); // Disable Motor A
+  digitalWrite(motorBEnablePin, LOW); // Disable Motor B
+
+  delay(1000); // Pause for 1 second
+}
+
+void MoveRight()
+{
+
+}
+
+void MoveLeft()
+{
+
+}
+
+void MoveForward()
+{
+
+}
+
+void Stop()
+{
+
+  // Initialize motors to stop
+  digitalWrite(motorAEnablePin, LOW);
+  digitalWrite(motorAPin1, LOW);
+  digitalWrite(motorAPin2, LOW);
+  digitalWrite(motorBEnablePin, LOW);
+  digitalWrite(motorBPin1, LOW);
+  digitalWrite(motorBPin2, LOW);
+}
+}
